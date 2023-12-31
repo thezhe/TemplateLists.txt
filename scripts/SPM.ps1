19b0648281a5299abd271cd582af77ee2f9e926d
Zhe Deng <42720670+thezhe@users.noreply.github.com>
2023-12-31 00:22:58 -0500

    try absolute path
    Signed-off-by: Zhe Deng <42720670+thezhe@users.noreply.github.com>

--- scripts/SPM.ps1
+++ scripts/SPM.ps1
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Script Package Manager.
-# Run a url as a script in Bash/PowerShell.
+# Run a url (stored in $SPM_URL) as a script in Bash/PowerShell.
 # See https://stackoverflow.com/questions/3362920/get-just-the-filename-from-a-path-in-a-bash-script.
 
 # Bash
