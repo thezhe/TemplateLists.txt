c3c64410f5dfa6087d3f34ea3b487fdb01e88307
Zhe Deng <42720670+thezhe@users.noreply.github.com>
2024-01-15 00:38:05 +0000

    remove cli check for case of passing in -D flags
    Signed-off-by: Zhe Deng <42720670+thezhe@users.noreply.github.com>

--- template.cmake
+++ template.cmake
@@ -9,13 +9,6 @@ Fatal if: reading fails.
 # See `execute_process` COMMAND_ERROR_IS_FATAL.
 cmake_minimum_required(VERSION 3.19 FATAL_ERROR)
 
-# CLI
-set(TEMPLATE_ARGC 3)
-
-if(NOT ${CMAKE_ARGC} EQUAL ${TEMPLATE_ARGC})
-    message(FATAL_ERROR "Usage: cmake -P  <path-to-template.cmake>")
-endif()
-
 # Read
 file(READ "test.txt" TEST_TXT)
 execute_process(
